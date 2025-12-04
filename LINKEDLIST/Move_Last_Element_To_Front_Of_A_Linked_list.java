0b690497456b6a911e5d3a1d00f4a201b853dbca
VIVEK ADABALA <adabalavivek887@gmail.com>
2025-12-04 05:55:57 +0530

    Add files via upload

---
+++ LINKEDLIST/Move_Last_Element_To_Front_Of_A_Linked_list.java
@@ -0,0 +1,42 @@
+package linkedlist;
+
+public class Move_Last_Element_To_Front_Of_A_Linked_list 
+{
+	class Node
+	{
+	    int data;
+	    Node next;
+
+	    Node(int x){
+	        data = x;
+	        next = null;
+	    }
+	}
+
+	
+
+	class Solution {
+	    public static Node moveToFront(Node head) {
+	        
+	        Node newNode=null;
+	        Node prev=null;
+	        Node temp=head;
+	        Node last=null;
+	        while(temp!=null &&temp.next!=null)
+	        {
+	            
+	            prev=temp;
+	            temp=temp.next;
+	              
+	        }
+	        
+	        prev.next=last;
+	        newNode= temp;
+	        newNode.next=head;
+	        head=newNode;
+	        return head;
+	        
+	    }
+	}
+
+}
