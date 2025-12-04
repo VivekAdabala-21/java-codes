232561be761626e3f2ce97e493aa52bec225d8db
VIVEK ADABALA <adabalavivek887@gmail.com>
2025-12-04 05:36:03 +0530

    Add files via upload

---
+++ LINKEDLIST/Reverse_An_Linkedlist.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+public class Reverse_An_Linkedlist 
+{
+	
+	     public class ListNode {
+	       int val;
+	       ListNode next;
+	       ListNode() {}
+	       ListNode(int val) { this.val = val; }
+	       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	     }
+	
+	
+	    public ListNode reverseList(ListNode head) 
+	    {
+	     ListNode previous=null;
+	     ListNode current=head;
+	     ListNode next=null;
+
+	        while(current!=null)
+	        {
+	            
+	            next=current.next;
+	            current.next=previous;
+	            previous=current;
+	            current=next;
+	        }   
+	        return previous;
+	    }  	
+}
